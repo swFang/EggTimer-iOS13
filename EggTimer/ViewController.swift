b9609e8cbd733f6d89428068301bb0bf02671412
Daniel Fang <danielfang@hotmail.com>
2020-12-26 14:56:11 -0800

    completed egg timer. Added in progress bar, alarm sound when timer is done, and title label changes depending on state of app. Also added in invalidate for timer when we start another egg

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,36 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    struct EggTimers {
-        static let soft = 5.0 * 60.0
-        static let medium = 8.0 * 60.0
-        static let hard = 12.0 * 60.0
-    }
+
+    let EggTimers = ["Soft": 5.0 * 60.0 , "Medium": 8.0 * 60.0, "Hard": 12.0 * 60.0]
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    // init to -1 so we know timer didnt start
+    var timeRemaining = -1.0
+    var totalTime = -1.0
+    var timer = Timer()
+    var player: AVAudioPlayer?
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        self.timer.invalidate()
+        progressBar.progress = 0.0
+        titleLabel.text = "Cooking in Progress"
+        totalTime = EggTimers[sender.currentTitle!]!
         startTimer(hardness: sender.currentTitle!)
     }
     
     func startTimer(hardness: String) {
-        switch hardness {
-        case "Hard":
-            Timer.scheduledTimer(timeInterval: EggTimers.hard * 60.0, target: self, selector: #selector(timerEnded), userInfo: nil, repeats: false)
-            print("hard timer started")
-        case "Medium":
-            Timer.scheduledTimer(timeInterval: EggTimers.medium, target: self, selector: #selector(timerEnded), userInfo: nil, repeats: false)
-            print("medium timer started")
-        default:
-            Timer.scheduledTimer(timeInterval: EggTimers.soft, target: self, selector: #selector(timerEnded), userInfo: nil, repeats: false)
-            print("soft timer started")
+        timeRemaining = EggTimers[hardness]!
+        
+        self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(stepTimer), userInfo: nil, repeats: true)
+    }
+
+    @objc func stepTimer() {
+        self.timeRemaining = self.timeRemaining - 1
+        let percentage = progressPercentage()
+        progressBar.progress = percentage
+
+        if (self.timeRemaining <= 0) {
+            timerComplete()
         }
     }
     
-    @objc func timerEnded() {
-        // play noise here
-        print("timer ended")
+    func progressPercentage() -> Float {
+        return 1.0 - (Float(self.timeRemaining) / Float(self.totalTime))
+    }
+    
+    func timerComplete() {
+        self.timer.invalidate()
+        titleLabel.text = "Egg Complete!"
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
     }
     
 }
