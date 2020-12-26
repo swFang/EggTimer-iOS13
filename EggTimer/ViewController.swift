956d5d941365ae938a3dba4ef36733f9aadd453d
Daniel Fang <danielfang@hotmail.com>
2020-12-26 13:35:17 -0800

    Completed control flow for basic app, need to add in Alarm, live timer, and clean up code

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    struct EggTimers {
+        static let soft = 5.0 * 60.0
+        static let medium = 8.0 * 60.0
+        static let hard = 12.0 * 60.0
+    }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        startTimer(hardness: sender.currentTitle!)
+    }
+    
+    func startTimer(hardness: String) {
+        switch hardness {
+        case "Hard":
+            Timer.scheduledTimer(timeInterval: EggTimers.hard * 60.0, target: self, selector: #selector(timerEnded), userInfo: nil, repeats: false)
+            print("hard timer started")
+        case "Medium":
+            Timer.scheduledTimer(timeInterval: EggTimers.medium, target: self, selector: #selector(timerEnded), userInfo: nil, repeats: false)
+            print("medium timer started")
+        default:
+            Timer.scheduledTimer(timeInterval: EggTimers.soft, target: self, selector: #selector(timerEnded), userInfo: nil, repeats: false)
+            print("soft timer started")
+        }
+    }
+    
+    @objc func timerEnded() {
+        // play noise here
+        print("timer ended")
+    }
+    
 }
